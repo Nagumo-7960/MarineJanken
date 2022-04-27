4828c6c7d0f74cf3ee34c58cdb991e671990cede
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-04-27 11:47:06 +0900

    developを作成

--- MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
+++ MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.marinejanken
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.marinejanken.ui.theme.MarineJankenTheme
 
@@ -21,29 +16,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            MarineJankenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
     }
 }
 
-data class Message(val author:String, val body:String)
-
 @Composable
-fun MessageCard(msg:Message){
-    Row{
-        androidx.compose.foundation.Image(painter = painterResource(id = R.drawable.girl_01), contentDescription = "Content profile picture")
-
-    }
-    Column() {
-        Text(text = msg.author)
-        Text(text = msg.body)
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PreviewMessageCard(){
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+fun DefaultPreview() {
+    MarineJankenTheme {
+        Greeting("Android")
+    }
 }
