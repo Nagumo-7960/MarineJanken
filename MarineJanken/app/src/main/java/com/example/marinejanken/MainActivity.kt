48027ee714e6fb71c2f18dc6fbd2ea8097344e53
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-04-27 15:08:08 +0900

    fillMaxSizeを実装

--- MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
+++ MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
@@ -8,11 +8,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -23,61 +24,87 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            Counter()
         }
     }
-}
 
-data class Message(val author: String, val body: String)
+    data class Message(val author: String, val body: String)
 
-@Composable
-fun MessageCard(msg: Message) {
-    Row {
-        androidx.compose.foundation.Image(
-            painter = painterResource(id = R.drawable.marine),
-            contentDescription = "Content profile picture"
-        )
-
-    }
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
-    }
-}
-
-@Composable
-fun Message2(username: String, body: String) {
-    Column {
+    @Composable
+    fun MessageCard(msg: Message) {
         Row {
-            Icon(Icons.Default.Person, contentDescription = null)
-            Text(text = username)
+            androidx.compose.foundation.Image(
+                painter = painterResource(id = R.drawable.marine),
+                contentDescription = "Content profile picture"
+            )
+
         }
-        Text(text = body)
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
-}
 
-@Composable
-fun Greeting() {
-    Text(
-        text = "Hello, world!",
-        modifier = Modifier
-            .clickable {  }
-            .background(color = Color.Gray)
-            .border(2.dp, Color.Red)
-            .padding(8.dp)
+    @Composable
+    fun Message2(username: String, body: String) {
+        Column {
+            MyButton()
+            Row {
+                Icon(Icons.Default.Person, contentDescription = null)
+                Text(text = username)
+            }
+            Text(text = body)
+        }
+    }
 
+    @Composable
+    fun Greeting() {
+        Text(
+            text = "Hello, world!",
+            modifier = Modifier
+                .clickable { }
+                .background(color = Color.Gray)
+                .border(2.dp, Color.Red)
+                .padding(8.dp)
+        )
+    }
+
+    @Composable
+    fun MyButton() {
+        Button(
+            onClick = { println("Hello, world!") }
+        ) {
+            Text(text = "Click")
+        }
+    }
+
+    @Composable
+    fun Counter() {
+        var count by remember { mutableStateOf(0) }
+        Column (                modifier = Modifier
+            .fillMaxSize()){
+            Text(
+                text = "Count:$count",
+
+                )
+            Button(
+                onClick = { count++ }
+            ) {
+                Text(text = "Click")
+            }
+        }
+    }
+
+
+    @Preview(
+        showBackground = true
     )
-}
-
-
-@Preview(
-    showBackground = true
-)
-@Composable
-fun PreviewMessageCard() {
+    @Composable
+    fun PreviewMessageCard() {
 //    MessageCard(
 //        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
 //    )
 //    Message2(username = "Hello", body = "Android")
-    Greeting()
+        Counter()
+    }
 }
