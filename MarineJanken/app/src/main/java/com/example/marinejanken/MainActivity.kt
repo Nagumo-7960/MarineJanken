2cf2fd93baf519d931576a0f9408e303b7cfc1b0
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-04-29 22:17:48 +0900

    文字の大きさと位置を調整

--- MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
+++ MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +56,11 @@ class MainActivity : ComponentActivity() {
         Text(
             text = "うみべのじゃんけん",
             modifier = Modifier
+                .fillMaxSize()
                 .padding(8.dp),
-            Color.White
+            Color.White,
+            fontSize = 33.sp,
+            textAlign = TextAlign.Center
         )
     }
 
