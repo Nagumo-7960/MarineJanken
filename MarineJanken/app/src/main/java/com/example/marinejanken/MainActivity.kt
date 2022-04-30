fcc4b0ccfb36e580c783f46a9d2fc6832569c68e
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-04-30 23:12:09 +0900

    スタートボタンの大きさ、色、角を調整

--- MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
+++ MarineJanken/app/src/main/java/com/example/marinejanken/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -80,11 +83,19 @@ class MainActivity : ComponentActivity() {
             onClick = { /*TODO*/ },
             modifier = Modifier
                 .padding(
-                    top = 200.dp
+                    top = 300.dp
                 )
+                .height(50.dp)
+                .width(120.dp),
+            shape = RoundedCornerShape(30),
+            colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.White,
+                contentColor = Color.Black
+            )
         ) {
             Text(
-                text = "スタート"
+                text = "スタート",
+                fontWeight = FontWeight.Bold
             )
         }
     }
