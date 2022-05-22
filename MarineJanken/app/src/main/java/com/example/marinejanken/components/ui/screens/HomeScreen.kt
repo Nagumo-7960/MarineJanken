7ccba80e2e20e7ea3dee0b9bc4bc526766ef315b
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-22 23:13:06 +0900

    ホーム画面のプレビューを追加

--- MarineJanken/app/src/main/java/com/example/marinejanken/components/ui/screens/HomeScreen.kt
+++ MarineJanken/app/src/main/java/com/example/marinejanken/components/ui/screens/HomeScreen.kt
@@ -14,9 +14,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.marinejanken.R
 
 @Composable
@@ -81,5 +83,12 @@ fun HomeImageCard() {
         contentScale = ContentScale.FillBounds,
         modifier = Modifier.fillMaxSize()
     )
+}
+
+@Preview
+@Composable
+fun PreviewHome() {
+    val navController = rememberNavController()
+    HomeScreen(navController)
 }
 
