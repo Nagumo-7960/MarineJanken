328d1f61e4368c1f76fe582eeb9a9f81c967bc8c
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-08 23:44:14 +0900

    Unit Testの導入

---
+++ MarineJanken/app/src/test/java/com/example/marinejanken/ui/screens/BattleResultScreenKtTest.kt
@@ -0,0 +1,18 @@
+package com.example.marinejanken.ui.screens
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BattleResultScreenKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun getBattleResult() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getOppResultPhrase() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getMyResultPhrase() {
+    }
+}
