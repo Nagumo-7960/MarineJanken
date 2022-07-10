3b6a4c918866af4e7d7924c4b10c6e889322ebe6
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-10 15:20:43 +0900

    じゃんけんの勝敗チェックをするテストを実行

--- MarineJanken/app/src/test/java/com/example/marinejanken/ui/screens/BattleResultScreenKtTest.kt
+++ MarineJanken/app/src/test/java/com/example/marinejanken/ui/screens/BattleResultScreenKtTest.kt
@@ -1,9 +1,20 @@
 package com.example.marinejanken.ui.screens
 
+import com.example.marinejanken.components.ui.screens.BattleResultCheck
+import com.example.marinejanken.components.ui.screens.battleResult
+import com.example.marinejanken.components.ui.screens.myHandCheck
+import com.example.marinejanken.components.ui.screens.oppHandCheck
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class BattleResultScreenKtTest {
+    @Test
+    fun JankenResultTest(){
+        myHandCheck = "gu"
+        oppHandCheck = "choki"
+        BattleResultCheck()
+        assertEquals("win", battleResult)
+    }
 
     @Test
     fun sampleTest(){
